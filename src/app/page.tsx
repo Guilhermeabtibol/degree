f9a346d5e2a292a240d7f6fa89b3e2aa3797c3bf
guilhermeabtibol <guiabtibol@gmail.com>
2025-03-12 16:24:49 -0400

    Adicionando direcionamento para os navslinks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,43 +15,43 @@ const Page = () => {
   return (
     <div>
       {/* Cabeçalho */}
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8 mx-auto" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <p className='font-serif font-bold h-8 w-auto text-white hover:text-gray-500'>Degree</p>
-            </a>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            <a href="#services" className="text-sm font-semibold text-white hover:text-gray-500">Sobre</a>
-            <a href="#" className="text-sm font-semibold text-white hover:text-gray-500">Projetos</a>
-            <a href="#" className="text-sm font-semibold text-white hover:text-gray-500">Localização</a>
-            <a href="#" className="text-sm font-semibold text-white hover:text-gray-500">Contato</a>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold text-white"><span aria-hidden="true">&rarr;</span></a>
-          </div>
+        <header className="absolute inset-x-0 top-0 z-50">
+          <nav className="flex items-center justify-between p-6 lg:px-8 mx-auto" aria-label="Global">
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <p className='font-serif font-bold h-8 w-auto text-white hover:text-gray-500'>Degree</p>
+              </a>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              <a href="#services" className="text-sm font-semibold text-white hover:text-gray-500">Sobre</a>
+              <a href="#portfolio" className="text-sm font-semibold text-white hover:text-gray-500">Projetos</a>
+              <a href="#localizacao" className="text-sm font-semibold text-white hover:text-gray-500">Localização</a>
+              <a href="#contato" className="text-sm font-semibold text-white hover:text-gray-500">Contato</a>
+            </div>
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a href="#" className="text-sm font-semibold text-white"><span aria-hidden="true">&rarr;</span></a>
+            </div>
 
-          {/* Botão do Menu para Mobile */}
-          <div className="lg:hidden">
-            <button onClick={toggleMenu} className="text-gray-500 hover:text-gray-900 focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-              </svg>
-            </button>
-          </div>
-        </nav>
-      </header>
+            {/* Botão do Menu para Mobile */}
+            <div className="lg:hidden">
+              <button onClick={toggleMenu} className="text-gray-500 hover:text-gray-900 focus:outline-none">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+              </button>
+            </div>
+          </nav>
+        </header>
 
       {/* Menu Mobile */}
       {isMenuOpen && (
         <div className="lg:hidden bg-gray-100 shadow-lg z-50">
           <div className="flex flex-col p-4 bg-black bg-opacity-50">
-            <a href="#" className="py-2 text-white hover:text-gray-300">Product</a>
-            <a href="#" className="py-2 text-white hover:text-gray-300">Features</a>
-            <a href="#" className="py-2 text-white hover:text-gray-300">Marketplace</a>
-            <a href="#" className="py-2 text-white hover:text-gray-300">Company</a>
+            <a href="#services" className="py-2 text-white hover:text-gray-300">Sobre</a>
+            <a href="#portfolio" className="py-2 text-white hover:text-gray-300">Projetos</a>
+            <a href="#localizacao" className="py-2 text-white hover:text-gray-300">Localização</a>
+            <a href="#contato" className="py-2 text-white hover:text-gray-300">Contato</a>
           </div>
         </div>
       )}
@@ -63,7 +63,7 @@ const Page = () => {
             <h1 className="text-5xl ml-16 font-serif font-semibold tracking-tight sm:text-7xl">BUSCAMOS ENTENDER O</h1>
             <h1 className="text-5xl ml-16 font-serif font-semibold tracking-tight sm:text-7xl">GOSTO DO CLIENTE</h1>
             <div className="mt-10 flex items-center gap-x-6">
-              <a href="#portfolio" className="rounded-md ml-16 bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+              < a href="#portfolio" className="rounded-md ml-16 bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
                 Ver Projetos
               </a>
             </div>
@@ -72,7 +72,7 @@ const Page = () => {
       </section>
 
       {/* Seção de Serviços */}
-      <section id="services" className=" py-20 justify-items-center bg-gray-100">
+      <section id="services" className="py-20 justify-items-center bg-gray-100">
         <div className="container mx-auto text-gray-800">
           <h3 className="mb-4 ml-1 text-3xl font-serif font-bold text-left mx-auto">O QUE FAZEMOS?</h3>
           <p className='text-left ml-1 text-content-justify text-2xl mx-auto'>
@@ -125,7 +125,7 @@ const Page = () => {
           <h3 className="text-3xl font-bold font-serif text-center">Projetos Realizados </h3>
           <p className="mt-4 text-center">Confira alguns dos nossos projetos mais recentes.</p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
-            <div className="bg-white p-4 rounded shadow mx-auto">
+            <div className="bg-white p-4 rounded shadow mx auto">
               <img src="/images/foto1.jpg" alt="Projeto 1" className="w-full h-48 object-cover rounded" />
               <h4 className="font-semibold mt-2">Projeto 1</h4>
             </div>
@@ -156,7 +156,7 @@ const Page = () => {
           </div>
           
           {/* Mapa à direita */}
-          <div className="md:w-1/2 mb-4 mx-auto">
+          <div className="md:w-1/2 mr-auto mx-auto">
             <iframe
               title="Localização no Google Maps"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.123456789012!2d-47.123456789012!3d-23.123456789012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1234567890abcdef!2sNome%20da%20Empresa!5e0!3m2!1spt-BR!2sbr!4v1234567890123"
